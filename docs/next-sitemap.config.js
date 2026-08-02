9a1c8c4e44c8aab4fdbdf1707cb3fd986afd57c3
Muhammad Zeeshan <iamzeeshan1233@gmail.com>
2026-08-02 22:01:24 +0500

    chore(seo): add Google Search Console verification and sitemap support

---
+++ docs/next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://subdomainx.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+}
--- docs/pages/_app.jsx
+++ docs/pages/_app.jsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="WbiK6VlkvLQnT7HkyZh9D5eZsLb9lyANzTZ_C0Ieup8" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+)
 }
